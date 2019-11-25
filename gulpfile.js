5173bcff6a9f083db875dc6795dd8e1373ec06c9
Ken Hawkins <khawkins98@gmail.com>
2019-11-25 11:23:39 +0100

    Update to vf-core beta.6
    Brings a much streamlined build+config

--- gulpfile.js
+++ gulpfile.js
@@ -1,119 +1,36 @@
-const gulp  = require('gulp');
-var   fractalBuildMode;
-
-// -----------------------------------------------------------------------------
-// Configuration
-// -----------------------------------------------------------------------------
-
-// Pull in some optional configuration from the package.json file, a la:
-// "vfConfig": {
-//   "vfName": "My Component Library",
-//   "vfNameSpace": "myco-",
-//   "vfComponentPath": "./src/components",
-//   "vfComponentDirectories": [
-//      "vf-core-components",
-//      "../node_modules/your-optional-collection-of-dependencies"
-//     NOTE: Don't forget to symlink: `cd components` `ln -s ../node_modules/your-optional-collection-of-dependencies`
-//    ],
-//   "vfBuildDestination": "./build",
-//   "vfThemePath": "@frctl/mandelbrot"
-// },
-// all settings are optional
-// todo: this could/should become a JS module
-const fs = require('fs');
 const path = require('path');
-const config = JSON.parse(fs.readFileSync('./package.json'));
-const vfCoreConfig = JSON.parse(fs.readFileSync(require.resolve('@visual-framework/vf-core/package.json')));
-config.vfConfig = config.vfConfig || [];
-global.vfName = config.vfConfig.vfName || "Visual Framework 2.0";
-global.vfNamespace = config.vfConfig.vfNamespace || "vf-";
-global.vfComponentPath = config.vfConfig.vfComponentPath || path.resolve('.', __dirname + '/components');
-global.vfBuildDestination = config.vfConfig.vfBuildDestination || __dirname + '/temp/build-files';
-global.vfThemePath = config.vfConfig.vfThemePath || './tools/vf-frctl-theme';
-global.vfVersion = vfCoreConfig.version || 'not-specified';
-const componentPath = path.resolve('.', global.vfComponentPath).replace(/\\/g, '/');
-const componentDirectories = config.vfConfig.vfComponentDirectories || ['vf-core-components'];
-const buildDestionation = path.resolve('.', global.vfBuildDestination).replace(/\\/g, '/');
+const gulp = require('gulp');
+
+// Pull in optional configuration from the package.json file, a la:
+const {componentPath, componentDirectories, buildDestionation} = require('@visual-framework/vf-config');
 
 // Tasks to build/run vf-core component system
 require('./node_modules/\@visual-framework/vf-core/tools/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
+require('./node_modules/\@visual-framework/vf-extensions/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
 
 // Watch folders for changess
 gulp.task('watch', function() {
-  gulp.watch(['./src/components/**/*.scss', '!./src/components/**/package.variables.scss'], gulp.parallel('vf-css'));
-  gulp.watch(['./src/components/**/*.js'], gulp.parallel('vf-scripts'));
-});
-
-gulp.task('set-to-development', function(done) {
-  process.argv.push('--serve');
-  process.env.ELEVENTY_ENV = 'development';
-  fractalBuildMode = 'server';
-  done();
-});
-
-gulp.task('set-to-static-build', function(done) {
-  process.argv.push('--quiet');
-  process.env.ELEVENTY_ENV = 'production';
-  fractalBuildMode = 'build';
-  // todo: switch build mode once vf-core beta.4 is out
-  // fractalBuildMode = 'dataobject'; // run fractal in server mode as there's no need for static html assets
-  done();
-});
-
-// Run eleventy, but only after we wait for fractal to bootstrap
-// @todo: consider if this could/should be two parallel gulp tasks
-gulp.task('eleventy', function(done) {
-  let elev;
-  process.argv.push('--config=eleventy.js'); // Eleventy config
-  global.vfBuilderPath   = __dirname + '/build/vf-core-components';
-  global.vfDocsPath      = __dirname + '/node_modules/\@visual-framework/vf-eleventy--extensions/fractal/docs';
-  global.vfOpenBrowser   = false; // if you want to open a browser tab for the component library
-  global.fractal         = require('@visual-framework/vf-core/fractal.js').initialize(fractalBuildMode,fractalReadyCallback); // make fractal components are available gloablly
-
-  function fractalReadyCallback(fractal) {
-    global.fractal = fractal; // save fractal globally
-    elev = require('./node_modules/\@visual-framework/vf-eleventy--extensions/11ty/cmd.js');
-    console.log('Done building Fractal');
-    buildEleventy();
-  }
-
-  function buildEleventy() {
-    if (process.env.ELEVENTY_ENV == 'production') {
-      elev.write().then(function() {
-        console.log('Done building 11ty');
-        done();
-      });
-    }
-    if (process.env.ELEVENTY_ENV == 'development') {
-      elev.watch().then(function() {
-        elev.serve('3000');
-        // console.log('Done building 11ty');
-        done();
-      });
-    }
-  }
-
-});
-
-// Eleventy doesn't always finish promptly, this ensures we exit gulp "cleanly"
-gulp.task('manual-exit', function(done) {
-  done()(process.exit());
+  // left for convience for local watch additions
+  gulp.watch(['./build/css/styles.css'], gulp.series('eleventy:reload'));
 });
 
 // Let's build this sucker.
 gulp.task('build', gulp.series(
   'vf-clean',
   gulp.parallel('vf-css','vf-scripts','vf-component-assets'),
-  'set-to-static-build',
-  'eleventy',
-  'manual-exit'
+  'fractal:build',
+  'fractal',
+  'eleventy:init',
+  'eleventy:build'
 ));
 
 // Build and watch things during dev
 gulp.task('dev', gulp.series(
   'vf-clean',
   gulp.parallel('vf-css','vf-scripts','vf-component-assets'),
-  'set-to-development',
-  'eleventy',
+  'fractal:development',
+  'fractal',
+  'eleventy:init',
+  'eleventy:develop',
   gulp.parallel('watch','vf-watch')
 ));
